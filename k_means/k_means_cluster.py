93ede09bbba3d436f3c1d8c24d87879440d0ca6f
Harish Mohan Raj <hraj@sapient.com>
2017-09-20 06:21:03 +0100

    k means completed

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -42,12 +42,17 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
+res = []
+for d in data_dict:
+    res.append(data_dict[d]['salary'])
+print sorted(res)
 
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
@@ -58,13 +63,16 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2, f3 in finance_features:
+    plt.scatter( f1, f2, f3 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+clf = KMeans(n_clusters=2)
+clf.fit(finance_features)
+pred = clf.predict(finance_features)
 
 
 
