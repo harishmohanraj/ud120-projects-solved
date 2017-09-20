9bc0cc4514f1ca7cdf1bfca69a975ceb0613fbe2
Harish Mohan Raj <hraj@sapient.com>
2017-09-20 05:45:45 +0100

    outliners completed

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,6 +25,18 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+# Target = net worth
+# Feature = age
+
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+
+print "What is the slope and the intercept?"
+print "Slope is: ",reg.coef_
+print "Intercept is: ",reg.intercept_
+score = reg.score(ages_test, net_worths_test)
+print "Regression Score on Tetsing data: ", score
 
 
 
@@ -77,8 +89,9 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
-
+    print "Slope is--------------: ",reg.coef_
+    score = reg.score(ages_test, net_worths_test)
+    print "Regression Score on Tetsing data: ", score
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
